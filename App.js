c84d1df38b570a763f78e4a7cf5f317f65bfd4bb
astevenaa <astevenaa@gmail.com>
2021-01-24 13:53:34 -0700

    added react navigator componenets

--- App.js
+++ App.js
@@ -1,10 +1,72 @@
 // import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { Button, Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Screen from './app/componets/Screen';
 
-import ListingEditScreen from './app/screens/ListingEditScreen';
+const Link = () => {
+  const navigation = useNavigation();
+  return <Button title="Click" onPress={() => navigation.navigate('Tweets')} />;
+};
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Button
+      title="Click"
+      onPress={() => navigation.navigate('TweetDetails', { id: 1 })}
+    />
+  </Screen>
+);
+
+const TweetDetails = ({ route }) => (
+  <Screen>
+    <Text>Tweet Details {route.params.id} </Text>
+  </Screen>
+);
+const Stack = createStackNavigator();
+
+const StackNavigator = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: 'dodgerblue ' },
+      headerTintColor: 'white',
+    }}
+  >
+    <Stack.Screen
+      name="Tweets"
+      component={Tweets}
+      options={{
+        headerStyle: { backgroundColor: 'tomato' },
+        headerTintColor: 'white',
+        headerShown: false,
+      }}
+    />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+  </Stack.Navigator>
+);
+
+const Account = () => (
+  <Screen>
+    <Text> Account</Text>
+  </Screen>
+);
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Feed" component={Tweets} />
+    <Tab.Screen name="Account" component={Account} />
+  </Tab.Navigator>
+);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
+  );
 }
 
 // secureTextEntry
