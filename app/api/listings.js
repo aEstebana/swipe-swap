c25ed0337530e964c92efbf905c5c39418c134de
astevenaa <astevenaa@gmail.com>
2021-01-31 14:59:58 -0700

    showing done animation and resetting the form using formik

--- app/api/listings.js
+++ app/api/listings.js
@@ -2,7 +2,7 @@ import client from './client';
 
 const endpoint = '/listings';
 const getListings = () => client.get(endpoint);
-const addListing = (listing) => {
+const addListing = (listing, onUploadProgress) => {
   const data = new FormData();
   data.append('title', listing.title);
   data.append('price', listing.price);
@@ -18,7 +18,8 @@ const addListing = (listing) => {
   if (listing.location)
     data.append('location', JSON.stringify(listing.location));
   return client.post(endpoint, data, {
-    onUploadProgress: (progress) => console.log(progress),
+    onUploadProgress: (progress) =>
+      onUploadProgress(progress.loaded / progress.total),
   });
 };
 export default { addListing, getListings };
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import * as Yup from 'yup';
 import {
@@ -12,6 +12,7 @@ import Screen from '../componets/Screen';
 import AppFormImagePicker from '../componets/forms/AppFormImagePicker';
 import useLocation from '../hooks/useLocation';
 import listingsApi from '../api/listings';
+import UploadScreen from './UploadScreen';
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label('Title'),
@@ -65,14 +66,30 @@ const categories = [
 ];
 function ListingEditScreen() {
   const location = useLocation();
+  const [uploadVisible, setUploadVisible] = useState(false);
+  const [progress, setProgress] = useState(0);
 
-  const handleSumbit = async (listing) => {
-    const result = await listingsApi.addListing({ ...listing, location });
-    if (!result.ok) return alert('count not post listing');
-    alert('succes');
+  const handleSumbit = async (listing, { resetForm }) => {
+    setProgress(0);
+    setUploadVisible(true);
+    const result = await listingsApi.addListing(
+      { ...listing, location },
+      (progress) => setProgress(progress)
+    );
+
+    if (!result.ok) {
+      setUploadVisible(false);
+      return alert('count not post listing');
+    }
+    resetForm();
   };
   return (
     <Screen style={styles.container}>
+      <UploadScreen
+        onDone={() => setUploadVisible(false)}
+        progress={progress}
+        visible={uploadVisible}
+      />
       <AppForm
         initialValues={{
           title: '',
