52eba9675f117c0d98a26186eaa0c93eda41d535
astevenaa <astevenaa@gmail.com>
2021-02-17 14:21:50 -0700

    fixed login page render

--- App.js
+++ App.js
@@ -1,54 +1,39 @@
-// import React, { useState } from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import AppLoading from 'expo-app-loading';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AppLoading from 'expo-app-loading';
 
-// import navigationTheme from './app/navigation/navigationTheme';
-// import OfflineNotice from './app/componets/OfflineNotice';
-// import AuthNavigator from './app/navigation/AuthNavigator';
-// //
-// import AuthContext from './app/auth/context';
-// //
-// import AppNavigator from './app/navigation/AppNavigator';
-// //
-// import authStorage from './app/auth/storage';
-// import { navigationRef } from './app/navigation/rootNavigation';
-import React from 'react';
-import { Button } from 'react-native';
-import Notifications from 'expo-notifications';
-import Screen from './app/componets/Screen';
+import navigationTheme from './app/navigation/navigationTheme';
+import OfflineNotice from './app/componets/OfflineNotice';
+import AuthNavigator from './app/navigation/AuthNavigator';
+import AppNavigator from './app/navigation/AppNavigator';
+import AuthContext from './app/auth/context';
+import authStorage from './app/auth/storage';
+import { navigationRef } from './app/navigation/rootNavigation';
 
-// const [user, setUser] = useState();
-// const [isReady, setIsReady] = useState(false);
-// const restoreToken = async () => {
-//   const token = authStorage.getUser();
-//   if (token) setUser(token);
-// };
-// if (!isReady)
-//   return (
-//     <AppLoading
-//       startAsync={restoreToken}
-//       onError={console.warn}
-//       onFinish={() => setIsReady(true)}
-//     />
-//   );
 export default function App() {
-  const showNotification = () => {
-    Notifications.scheduleNotificationAsync({
-      title: 'Congratulation',
-      body: 'your oder was placed ',
-      time: new Date().getTime() + 2000,
-    });
+  const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
+
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
   };
+  if (!isReady)
+    return (
+      <AppLoading
+        startAsync={restoreUser}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    );
+
   return (
-    <Screen>
-      <Button title="Tap me" onPress={showNotification} />
-    </Screen>
-    // <AuthContext.Provider value={{ user, setUser }}>
-    //   <OfflineNotice />
-    //   <NavigationContainer ref={navigationRef} theme={navigationTheme}>
-    //     {user ? <AppNavigator /> : <AuthNavigator />}
-    //   </NavigationContainer>
-    // </AuthContext.Provider>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <OfflineNotice />
+      <NavigationContainer ref={navigationRef} theme={navigationTheme}>
+        {user ? <AppNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
 
--- app/navigation/routes.js
+++ app/navigation/routes.js
@@ -1,3 +1,7 @@
 export default {
   LISTING_DETAILS: 'ListingDetails',
+  LISTING_EDIT: 'ListingEdit',
+  LOGIN: 'Login',
+  MESSAGES: 'Messages',
+  REGISTER: 'Register',
 };
